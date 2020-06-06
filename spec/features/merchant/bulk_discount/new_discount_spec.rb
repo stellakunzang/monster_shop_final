ef3abaa1f3566ccbf5619ffbb53b159aa40fb1c0
stellakunzang <stella.k.bonnie@gmail.com>
2020-06-05 23:02:39 -0600

    Add test for new discount form

--- spec/features/merchant/bulk_discount/new_discount_spec.rb
+++ spec/features/merchant/bulk_discount/new_discount_spec.rb
@@ -10,18 +10,10 @@ RSpec.describe 'New bulk discount' do
       @nessie = @merchant_1.items.create!(name: 'Nessie', description: "I'm a Loch Monster!", price: 20.25, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       @giant = @merchant_1.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: false, inventory: 3 )
       @hippo = @merchant_2.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 1 )
-      @order_1 = @m_user.orders.create!(status: "pending")
-      @order_2 = @m_user.orders.create!(status: "pending")
-      @order_3 = @m_user.orders.create!(status: "pending")
-      @order_item_1 = @order_1.order_items.create!(item: @hippo, price: @hippo.price, quantity: 2, fulfilled: false)
-      @order_item_2 = @order_2.order_items.create!(item: @hippo, price: @hippo.price, quantity: 2, fulfilled: true)
-      @order_item_3 = @order_2.order_items.create!(item: @ogre, price: @ogre.price, quantity: 2, fulfilled: false)
-      @order_item_4 = @order_3.order_items.create!(item: @giant, price: @giant.price, quantity: 4, fulfilled: false)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
     end
 
     it "I can click a link to a new bulk discount form" do
-
       visit "/merchant"
 
       click_link "Create New Bulk Discount"
@@ -30,6 +22,12 @@ RSpec.describe 'New bulk discount' do
     end
 
     it "I can create a discount for a merchant" do
+      visit "/merchant/discounts/new"
+
+      fill_in :percent_discount, with: 10
+      # select with check-box: 'Applicable Items', with: 'All'
+      fill_in :minimum_quantity, with: 5
+      click_button 'Create Discount'
     end
 
     it "I cannot create a discount without specifying both percent and either quantity or value" do
