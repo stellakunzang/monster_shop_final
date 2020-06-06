18b09c73559e8414ae012ca18211af1830fd4bec
stellakunzang <stella.k.bonnie@gmail.com>
2020-06-06 16:22:15 -0600

    Add validations to discount model and items_list method

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,5 +1,13 @@
 class Discount < ApplicationRecord
   has_many :discount_items
   has_many :items, through: :discount_items
-  belongs_to :merchant 
+  belongs_to :merchant
+
+  validates_presence_of :percent_discount
+  validates_presence_of :minimum_value, presence: true, unless: :minimum_quantity
+  validates_presence_of :minimum_quantity, presence: true, unless: :minimum_value
+
+  def items_list
+    items.pluck(:name).to_sentence 
+  end
 end
