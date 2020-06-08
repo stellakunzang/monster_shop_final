41984611f8845dd1b4cbe43b0cd81c3ea65eade0
stellakunzang <stella.k.bonnie@gmail.com>
2020-06-07 22:12:32 -0600

    Write tests for cart model methods to help find discount

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -63,5 +63,102 @@ RSpec.describe Cart do
 
       expect(@cart.count_of(@giant.id)).to eq(1)
     end
+
+    it '.cart_merchants' do
+      @cart.add_item(@hippo.id.to_s)
+
+      expect(@cart.cart_merchants).to eq([@megan.id, @brian.id])
+    end
+
+    it '.item_on_sale' do
+      discount = @megan.discounts.create!(percent_discount: 25.0, minimum_value: 100.0)
+      discount.discount_items.create!(item_id: @giant.id)
+
+      expect(@cart.item_on_sale(@giant, discount)).to eq(true)
+      expect(@cart.item_on_sale(@ogre, discount)).to eq(false)
+    end
+
+    it '.valid_discounts' do
+      expect(@cart.valid_discounts).to eq([])
+
+      discount = @megan.discounts.create!(percent_discount: 25.0, minimum_value: 50.0)
+      discount.discount_items.create!(item_id: @giant.id)
+      discount.discount_items.create!(item_id: @ogre.id)
+
+      expect(@cart.valid_discounts).to eq([discount])
+    end
+
+    it '.validate_attributes' do
+      discount = @megan.discounts.create!(percent_discount: 25.0, minimum_value: 100.0)
+      discount.discount_items.create!(item_id: @giant.id)
+      discount.discount_items.create!(item_id: @ogre.id)
+      expect(@cart.validate_attributes([discount])).to eq({discount.id => {quantity: 3, value: 120 }})
+    end
+
+    it '.discount_qualifications_met' do
+      discount = @megan.discounts.create!(percent_discount: 25.0, minimum_value: 100.0)
+      discount.discount_items.create!(item_id: @ogre.id)
+
+      expect(@cart.discount_qualifications_met([discount])).to eq([])
+
+      discount.discount_items.create!(item_id: @giant.id)
+      expect(@cart.discount_qualifications_met([discount])).to eq([discount])
+    end
+
+    it '.best_discount' do
+      @cart.add_item(@hippo.id.to_s)
+      @cart.add_item(@hippo.id.to_s)
+      discount = @megan.discounts.create!(percent_discount: 25.0, minimum_value: 100.0)
+      discount2 = @brian.discounts.create!(percent_discount: 50.0, minimum_value: 50.0)
+      discount2.discount_items.create!(item_id: @hippo.id)
+      discount.discount_items.create!(item_id: @giant.id)
+      discount.discount_items.create!(item_id: @ogre.id)
+
+      expect(@cart.best_discount).to eq(discount2)
+    end
+
+    it '.determine_discount' do
+      discount = @megan.discounts.create!(percent_discount: 25.0, minimum_value: 100.0)
+      discount.discount_items.create!(item_id: @giant.id)
+      discount.discount_items.create!(item_id: @ogre.id)
+
+      expect(@cart.determine_discount).to eq(discount)
+
+      @cart.add_item(@hippo.id.to_s)
+      @cart.add_item(@hippo.id.to_s)
+
+      discount2 = @brian.discounts.create!(percent_discount: 50.0, minimum_value: 50.0)
+      discount2.discount_items.create!(item_id: @hippo.id)
+
+      expect(@cart.determine_discount).to eq(discount2)
+    end
+
+    it '.discount_applied' do
+      expect(@cart.discount_applied).to eq(false)
+
+      discount = @megan.discounts.create!(percent_discount: 25.0, minimum_quantity: 3)
+      discount.discount_items.create!(item_id: @giant.id)
+      discount.discount_items.create!(item_id: @ogre.id)
+
+      expect(@cart.discount_applied).to eq(true)
+    end
+
+    it '.price_with_discount' do
+      discount = @megan.discounts.create!(percent_discount: 25.0, minimum_quantity: 3)
+      discount.discount_items.create!(item_id: @giant.id)
+      discount.discount_items.create!(item_id: @ogre.id)
+
+      expect(@cart.price_with_discount(@ogre.price)).to eq(15)
+    end
+
+    it 'subtotal_with_discount' do
+      discount = @megan.discounts.create!(percent_discount: 25.0, minimum_quantity: 3)
+      @cart.add_item(@ogre.id.to_s)
+      discount.discount_items.create!(item_id: @giant.id)
+      discount.discount_items.create!(item_id: @ogre.id)
+
+      expect(@cart.subtotal_with_discount(@ogre.id)).to eq(30)
+    end
+
   end
 end
